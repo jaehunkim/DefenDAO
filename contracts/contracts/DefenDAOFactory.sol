e9b40b6de7f8b41cd763d2e2db4de6389218594d
jaehunkim <jaehunkim@outlook.com>
2022-11-25 15:14:43 +0900

    Resolve confilcts

--- contracts/contracts/DefenDAOFactory.sol
+++ contracts/contracts/DefenDAOFactory.sol
@@ -14,7 +14,11 @@ contract DefenDAOFactory is IDefenDAOFactory, Ownable {
 
     constructor() Ownable() {}
 
-    function makeCollection(address token_) public override onlyOwner {
+    function makeCollection(
+        address token_,
+        uint256 curFloorPrice_,
+        uint256 offerPriceUnit_
+    ) public override onlyOwner {
         require(token_ != address(0), "token cannot be zero address");
         // TODO : check erc721
         require(getCollections[token_] == address(0), "collection exists");
@@ -24,7 +28,7 @@ contract DefenDAOFactory is IDefenDAOFactory, Ownable {
         assembly {
             col := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
-        // IDefenDAO(col).initialize(token_);
+        IDefenDAO(col).initialize(token_, curFloorPrice_, offerPriceUnit_);
         getCollections[token_] = col;
         collections.push(col);
         emit CollectionCreated(token_, col);
--- contracts/contracts/IDefenDAOFactory.sol
+++ contracts/contracts/IDefenDAOFactory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 interface IDefenDAOFactory {
 /// functions
 // - create new DefenDAO unit for specific NFT collection
-    function makeCollection(address) external;
+    function makeCollection(address, uint256, uint256) external;
     function getCollection(address) external returns (address);
     function getAllCollections() external returns (address[] memory);
 }
