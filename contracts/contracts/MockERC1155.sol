a859d943df1e968adbacfd133dbd1f1d2af7be5d
liayoo <lia@comcom.ai>
2022-11-27 20:28:42 +0900

    Fix duplicate MockERC721.

--- contracts/contracts/MockERC1155.sol
+++ contracts/contracts/MockERC1155.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract MockERC721 is ERC1155 {
+contract MockERC1155 is ERC1155 {
     constructor() ERC1155("test_uri") {}
 
     function mint(uint256 tokenId, uint256 amount) public {
